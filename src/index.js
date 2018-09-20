3420b422ec2ed95eac087a15bc258ac726333c3a
Arkadz <penkratpavel@gmail.com>
2018-09-20 21:07:30 +0300

    i have changed src/index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var arr = preferences;
+  arr.unshift(0);
+  var count=0;
+		for (let t=1; t<=arr.length; t++) {
+			if (
+				(arr[arr[arr[t]]] == t) && (arr[t]!=arr[arr[t]] && arr[arr[t]]!=arr[arr[arr[t]]])
+				) {count++;}
+
+      }
+      if (count==0) {return 0;}
+      return count/3;
 };
